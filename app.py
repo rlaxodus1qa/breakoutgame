c5692bc365a916a7190a26143c2b2c9e17598533
rlaxodus_123 <tgim82073@gmail.com>
2024-07-19 10:41:55 +0900

    Create canvaas

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Hello. World")
+import pygame
+
+pygame.init()
+
+beige_marka = (250, 237, 201)
+
+# 너비, 높이 설정 800x600
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+# 프로그램을 시작했을 떄 표시되는 제목
+pygame.display.set_caption("벽돌 꺠기 게임")
+
+# 메인 반복 구간
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 화면 배경 색 설정
+    screen.fill(beige_marka)
+
+    # 화면 업데이트
+    pygame.display.flip()
+
+# 종료 처리
+pygame.quit()
